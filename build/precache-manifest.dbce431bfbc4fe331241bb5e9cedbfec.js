ad645fe42fff748fa1d5a0c8dc7b461a4208157a
Sam Foster <sfoster@brandwatch.com>
2020-03-31 22:02:02 +0100

    Added frontend

---
+++ build/precache-manifest.dbce431bfbc4fe331241bb5e9cedbfec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797e5767e1f2edb1c85f23525cc704f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b65b0e6e5947f54bc9f",
+    "url": "/static/css/main.aad4c46b.chunk.css"
+  },
+  {
+    "revision": "47f83a40457bf323026e",
+    "url": "/static/js/2.1ed3cd79.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1ed3cd79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b65b0e6e5947f54bc9f",
+    "url": "/static/js/main.57fd3be6.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
